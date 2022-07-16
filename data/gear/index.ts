2ec6a67aa872f1c4c394e7f3f78c4bc08ff8a8f3
zyqq <zyqq.dev@gmail.com>
2022-07-16 22:53:25 +0800

    clean data/gear/index.ts

--- data/gear/index.ts
+++ data/gear/index.ts
@@ -1,26 +1,3 @@
-// const gear = [
-//   {
-//     ratio: 2.976190476,
-//     speed: null,
-//   },
-//   {
-//     ratio: 2.105263158,
-//     speed: null,
-//   },
-//   {
-//     ratio: 1.615508885,
-//     speed: null,
-//   },
-//   {
-//     ratio: 1.277139208,
-//     speed: null,
-//   },
-//   {
-//     ratio: 1.030927835,
-//     speed: null,
-//   },
-// ];
-
 const gear = [
   {
     ratio: 2.976190476,
