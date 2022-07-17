3298f1e9044836d0cd2f0d1286ee001f8390f99f
Haziq Hapiz <zyqq.dev@gmail.com>
2022-07-17 13:22:52 +0800

    refactor code round to whole number. add calculateRPM function

--- data/model/Tuning.ts
+++ data/model/Tuning.ts
@@ -54,6 +54,27 @@ class Tuning {
     }
   }
 
+  calculateRPM(
+    rpm: number,
+    ratio: number,
+    circumference: number,
+    finalDrive: number,
+  ): number {
+    let speed =
+      rpm * ((1 / ratio) * (1 / finalDrive) * circumference * 0.001 * 60);
+
+    // round speed to 2 decimal places
+    speed = Math.round(speed * 100) / 100;
+
+    // round speed to whole number if 2 decimal places is > 0.5
+    if (speed % 1 !== 0) {
+      speed = Math.round(speed);
+    } else {
+      speed = Math.floor(speed);
+    }
+    return speed;
+  }
+
   calculateSpeeds(finalDrive: number): void {
     let rpm: number[] = [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000];
 
@@ -61,25 +82,19 @@ class Tuning {
       let speeds: number[] = [];
       let ratioSpeeds: Map<Number, Number> = new Map<Number, Number>();
       for (let j = 0; j < rpm.length; j++) {
-        let speed =
-          rpm[j] *
-          ((1 / this.gears[i].getRatio()) *
-            (1 / finalDrive) *
-            this.tire.getCircumference() *
-            0.001 *
-            60);
-
-        // round speed to 2 decimal places
-        speed = Math.round(speed * 100) / 100;
+        let speed = this.calculateRPM(
+          rpm[j],
+          this.gears[i].getRatio(),
+          this.tire.getCircumference(),
+          finalDrive,
+        );
 
         // add speed to array
         speeds.push(speed);
-        // ratioSpeeds.set(speed, rpm[j]);
-        // rpm => speed
         ratioSpeeds.set(rpm[j], speed);
       }
 
-      // set gear speedss
+      // set gear speeds
       this.gears[i].setSpeeds(speeds);
       this.gears[i].setRatioSpeeds(ratioSpeeds);
     }
